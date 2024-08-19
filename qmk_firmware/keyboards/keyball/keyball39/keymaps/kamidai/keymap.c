090dc1f339a11cf1f93864c48e7d93465ab37c39
isshki025 <mono.color25@gmail.com>
2024-08-20 01:23:17 +0900

    コンパイルエラー修正

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                       KC_Y         , KC_U          , KC_I      , KC_O     , KC_P      ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                       KC_H         , KC_J          , KC_K      , KC_L     , KC_ENTER  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                       KC_N         , KC_M          , KC_COMMA  , KC_DOT   , KC_RCTL   ,
-    KC_LGUI  , KC_TRNS  , KC_TRNS  , KC_TRNS  , LT(1, KC_SPACE), LT(2,KC_LNG2),    LT(1,KC_LNG1),LT(2,KC_BSPC), LT(3, KC_TAB) , KC_RSHIFT , KC_RALT  , KC_RCTL
+    KC_LGUI  , KC_TRNS  , KC_TRNS  , KC_TRNS  , LT(1, KC_SPACE), LT(2,KC_LNG2),    LT(1,KC_LNG1),LT(2,KC_BSPC), LT(3, KC_TAB) , KC_RSFT   , KC_RALT  , KC_RCTL
   ),
 
   [1] = LAYOUT_universal(
