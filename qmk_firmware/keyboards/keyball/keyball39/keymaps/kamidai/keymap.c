a9c8a058c82f025d6a09bd24e8d2a98958ffd653
isshki025 <mono.color25@gmail.com>
2024-08-23 00:29:18 +0900

    oledサンプル

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
@@ -81,54 +81,54 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return state;
 }
 
-#ifdef OLED_ENABLE
+// #ifdef OLED_ENABLE
 
-#include "lib/oledkit/oledkit.h"
+// #include "lib/oledkit/oledkit.h"
 
-void oledkit_render_info_user(void) {
-  // デバッグ用に変数を表示する
-  // oled_write_P(PSTR("Debug:"), false);
-  // oled_write(get_u8_str(xxx, ' '), false);
+// void oledkit_render_info_user(void) {
+//   // デバッグ用に変数を表示する
+//   // oled_write_P(PSTR("Debug:"), false);
+//   // oled_write(get_u8_str(xxx, ' '), false);
 
-  keyball_oled_render_keyinfo();   // キー情報を表示
-  keyball_oled_render_ballinfo();  // トラックボール情報を表示
+//   keyball_oled_render_keyinfo();   // キー情報を表示
+//   keyball_oled_render_ballinfo();  // トラックボール情報を表示
 
-  // <Layer>を表示する
-  oled_write_P(PSTR("Layer:"), false);
-  oled_write(get_u8_str(get_highest_layer(layer_state), ' '), false);
+//   // <Layer>を表示する
+//   oled_write_P(PSTR("Layer:"), false);
+//   oled_write(get_u8_str(get_highest_layer(layer_state), ' '), false);
 
-  // <マウス移動量 / クリックレイヤーしきい値>を表示
-  // oled_write_P(PSTR(" MV:"), false);
-  // oled_write(get_u8_str(mouse_movement, ' '), false);
-  // oled_write_P(PSTR("/"), false);
-  // oled_write(get_u8_str(to_clickable_movement, ' '), false);
+//   // <マウス移動量 / クリックレイヤーしきい値>を表示
+//   // oled_write_P(PSTR(" MV:"), false);
+//   // oled_write(get_u8_str(mouse_movement, ' '), false);
+//   // oled_write_P(PSTR("/"), false);
+//   // oled_write(get_u8_str(to_clickable_movement, ' '), false);
 
-  // <state>を表示
-  switch (state) {
-    case WAITING:
-      oled_write_ln_P(PSTR("  WAITING"), false);
-      break;
-    case CLICKABLE:
-      oled_write_ln_P(PSTR("  CLICKABLE"), false);
-      break;
-    case CLICKING:
-      oled_write_ln_P(PSTR("  CLICKING"), false);
-      break;
-    case CLICKED:
-      oled_write_ln_P(PSTR("  CLICKED"), false);
-      break;
-    case SWIPE:
-      oled_write_ln_P(PSTR("  SWIPE"), false);
-      break;
-    case SWIPING:
-      oled_write_ln_P(PSTR("  SWIPING"), false);
-      break;
-    case NONE:
-      oled_write_ln_P(PSTR("  NONE"), false);
-      break;
-  }
-}
-#endif
+//   // <state>を表示
+//   switch (state) {
+//     case WAITING:
+//       oled_write_ln_P(PSTR("  WAITING"), false);
+//       break;
+//     case CLICKABLE:
+//       oled_write_ln_P(PSTR("  CLICKABLE"), false);
+//       break;
+//     case CLICKING:
+//       oled_write_ln_P(PSTR("  CLICKING"), false);
+//       break;
+//     case CLICKED:
+//       oled_write_ln_P(PSTR("  CLICKED"), false);
+//       break;
+//     case SWIPE:
+//       oled_write_ln_P(PSTR("  SWIPE"), false);
+//       break;
+//     case SWIPING:
+//       oled_write_ln_P(PSTR("  SWIPING"), false);
+//       break;
+//     case NONE:
+//       oled_write_ln_P(PSTR("  NONE"), false);
+//       break;
+//   }
+// }
+// #endif
 
 static const char PROGMEM meat_ball_logo_1[] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
