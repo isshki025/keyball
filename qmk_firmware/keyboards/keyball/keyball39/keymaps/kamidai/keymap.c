847396616c057a690311f4763bb7c1e0202f7b80
isshki025 <mono.color25@gmail.com>
2024-08-23 11:34:27 +0900

    中段にctrl、shift、altを追加する

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
@@ -10,7 +10,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                       KC_Y         , KC_U          , KC_I      , KC_O     , KC_P      ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                       KC_H         , KC_J          , KC_K      , KC_L     , KC_ENTER  ,
+    KC_A     , MT(MOD_LALT, KC_S)     , MT(MOD_LSFT, KC_D)     , MT(MOD_LCTL, KC_F) , KC_G     ,                                       KC_H         , MT(MOD_RCTL, KC_J)          , MT(MOD_RSFT, KC_K)      , MT(MOD_RALT, KC_L)     , KC_ENTER  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                       KC_N         , KC_M          , KC_COMMA  , KC_DOT   , KC_RCTL   ,
     KC_LGUI  , KC_TRNS  , KC_TRNS  , KC_TRNS  , LT(1, KC_SPACE), LT(2,KC_LNG2),    LT(1,KC_LNG1),LT(2,KC_BSPC), LT(3, KC_TAB) , KC_RSFT   , KC_RALT  , KC_RCTL
   ),
